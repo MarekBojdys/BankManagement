d69b3fc0b4974c59139662b12e7f031980aeb00a
Marek <marek.bojdys@ekinnolab.eu>
2020-05-22 14:21:03 +0200

    add field validations

--- src/main/java/pl/marekbojdys/bankmanagement/dtos/BankAccountAddRequestDTO.java
+++ src/main/java/pl/marekbojdys/bankmanagement/dtos/BankAccountAddRequestDTO.java
@@ -1,10 +1,16 @@
 package pl.marekbojdys.bankmanagement.dtos;
 
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
 public class BankAccountAddRequestDTO {
 
+    @NotBlank(message = "First name can not be blank")
     private String firstName;
+    @NotBlank(message = "Last name can not be blank")
     private String lastName;
+    @Min(value = 0, message = "Initial balance can not be less than 0")
     private Integer balance;
 
     public String getFirstName() {
